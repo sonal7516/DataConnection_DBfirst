c20642536a9304661340ae9493714fe5b88749f1
admin <admin@CHETUIWK820>
2022-05-02 17:12:55 +0530

    Add project files.

---
+++ DataConnection_DBfirst/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataConnection_DBfirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly LokeshEntities lokesh;
+        public HomeController()
+        {
+            lokesh = new LokeshEntities();
+        }
+
+        public ActionResult Index()
+        {
+            List<employee> emp = lokesh.employees.ToList();
+            return View(emp);
+        } 
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
